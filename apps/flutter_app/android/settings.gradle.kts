e6242e2d0c8f8d8679ba7806e9cb02fe16d7dd2a
root <root@localhost.localdomain>
2026-09-02 13:00:26 +0800

    Complete Flutter Android Gradle integration

--- apps/flutter_app/android/settings.gradle.kts
+++ apps/flutter_app/android/settings.gradle.kts
@@ -1,4 +1,9 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    val flutterSdkPath = System.getenv("FLUTTER_ROOT") ?: error("FLUTTER_ROOT is not set")
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+plugins { id("dev.flutter.flutter-plugin-loader") version "1.0.0" }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS); repositories { google(); mavenCentral() } }
 rootProject.name = "document_converter"
 include(":app")
